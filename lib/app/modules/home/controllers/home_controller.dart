bc66e73f37eba627efcfc3c2e5c8320f3d70f942
Ahmad Iqbal Fadhillah <11191004@student.itk.ac.id>
2022-12-06 20:59:45 +0800

    profile view

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,5 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
+import 'package:rentcarapp/app/routes/app_pages.dart';
 
 class HomeController extends GetxController {
   var tabIndex = 0.obs;
@@ -7,5 +10,17 @@ class HomeController extends GetxController {
   void changeTabIndex(int index) {
     tabIndex.value = index;
   }
+  FirebaseAuth auth = FirebaseAuth.instance;
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
 
+  Stream<DocumentSnapshot<Map<String, dynamic>>> streamUser() async* {
+    String uid = await auth.currentUser!.uid;
+
+    yield* firestore.collection("user").doc(uid).snapshots();
+  }
+
+  void logout() async {
+    await FirebaseAuth.instance.signOut();
+    Get.offAllNamed(Routes.LOGIN);
+  }
 }
--- lib/app/modules/home/views/profile_view.dart
+++ lib/app/modules/home/views/profile_view.dart
@@ -1,31 +1,120 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
 import '../controllers/home_controller.dart';
 
-class ProfileView extends StatelessWidget {
+class ProfileView extends GetView<HomeController> {
   const ProfileView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        height: size.height,
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Text(
-                "Profile Page",
-                style: TextStyle(fontSize: 24),
-              )
-            ],
-          ),
-        ),
+      body: StreamBuilder <DocumentSnapshot<Map<String, dynamic>>>(
+        stream: controller.streamUser(),
+        builder: (context, snap) {
+          if (snap.connectionState == ConnectionState.waiting) {
+            Center(child: CircularProgressIndicator(),);
+          }
+          if(snap.hasData) {
+            Map<String, dynamic> user = snap.data!.data()!;
+            return ListView(
+              padding: EdgeInsets.all(20),
+              children: [
+                SizedBox(height: 5,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ClipOval(
+                      child: Container(
+                        width: 120,
+                        height: 120,
+                        child: Image.network(
+                          "https://ui-avatars.com/api/?name=${user['email']}",
+                          fit: BoxFit.cover,),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20,),
+                Text(
+                  "${user['nama'].toString().toUpperCase()}",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.w500,
+                  ),
+                ),
+                Text(
+                  "${user['email']}",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 15,
+                  ),
+                ),
+                SizedBox(height: 30,),
+                ListTile(
+                  onTap: () {},
+                  leading: Icon(Icons.person),
+                  iconColor: Color(0xFFEF233C),
+                  title: Text("Update profile",),
+                  trailing: Icon(Icons.arrow_forward_ios, size: 20,),
+                  shape: RoundedRectangleBorder(
+                    side: BorderSide(color: Color(0xFFEF233C)),
+                    borderRadius: BorderRadius.circular(28)
+                  ),
+                ),
+                SizedBox(height: 8,),
+                ListTile(
+                  onTap: () {},
+                  leading: Icon(Icons.vpn_key),
+                  iconColor: Color(0xFFEF233C),
+                  title: Text("Ubah password"),
+                  trailing: Icon(Icons.arrow_forward_ios, size: 20,),
+                  shape: RoundedRectangleBorder(
+                    side: BorderSide(color: Color(0xFFEF233C)),
+                    borderRadius: BorderRadius.circular(28)
+                  ),
+                ),
+                SizedBox(height: 8,),
+                ListTile(
+                  onTap: () => controller.logout(),
+                  leading: Icon(Icons.logout),
+                  iconColor: Color(0xFFEF233C),
+                  title: Text("Logout"),
+                  trailing: Icon(Icons.arrow_forward_ios, size: 20,),
+                  shape: RoundedRectangleBorder(
+                    side: BorderSide(color: Color(0xFFEF233C)),
+                    borderRadius: BorderRadius.circular(28),
+                  ),
+                ),
+              ],
+            );
+          } else {
+            return Center(
+              child: Text("Tidak dapat memuat data user.",),
+            );
+          }
+        },
       ),
+      // body: Container(
+      //   width: double.infinity,
+      //   height: size.height,
+      //   child: Center(
+      //     child: Column(
+      //       mainAxisAlignment: MainAxisAlignment.center,
+      //       children: const [
+      //         Text(
+      //           "Profile Page",
+      //           style: TextStyle(fontSize: 24),
+      //         )
+      //       ],
+      //     ),
+      //   ),
+      // ),
     );
   }
 }
